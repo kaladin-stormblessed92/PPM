e8d603d29f6ab24719a4260461510a5144cc36e9
kaladin-stormblessed92 <kaladin.stromblessed.92@gmail.com>
2019-08-17 11:29:55 +0530

    PPM Initialized.
    Initialized PPM.

---
+++ src/main/java/com/kelsier/ppm/PpmApplication.java
@@ -0,0 +1,13 @@
+package com.kelsier.ppm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kelsier/ppm/PpmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kelsier.ppm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PpmApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
